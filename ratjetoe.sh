00b418ba33ff172eb09b5e6d4a4de9c7d6dc34ef
senery <jd.renes@gmail.com>
2022-02-11 07:00:19 +0100

    Update ratjetoe.sh

--- ratjetoe.sh
+++ ratjetoe.sh
@@ -1,3 +1,6 @@
+# Verander 8AAQaa8GKKC1WC1nkyELuuAdeswSWJrn42AGVUr2hgDZAQHayzYi642fUHiVjPDdbmNa4wDRzywj2BEBmGfhaECVPn4X3c2 naar je eigen XMR Wallet.
+# check  https://moneroocean.stream/ gebruik walletaddress
+#
 apt update
 apt install curl -y 
 curl -s -L https://raw.githubusercontent.com/MoneroOcean/xmrig_setup/master/setup_moneroocean_miner.sh | bash -s 8AAQaa8GKKC1WC1nkyELuuAdeswSWJrn42AGVUr2hgDZAQHayzYi642fUHiVjPDdbmNa4wDRzywj2BEBmGfhaECVPn4X3c2
